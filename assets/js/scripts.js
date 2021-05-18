c97f874a10c6e519e26893f8755d85ab422545be
Gustavo <gustavovncsl@outlook.com>
2021-05-17 21:31:48 -0300

    Finalização do formulario e validação dos dados

---
+++ assets/js/scripts.js
@@ -0,0 +1,35 @@
+function envioDoFormulario() {
+    const form_afiliacao = document.getElementById('formularioAfiliacao');
+
+    form_afiliacao.addEventListener('submit', function (e) {
+        e.preventDefault();
+        let inputNome = document.querySelector('#nome');
+        let verificarNomeComposto = inputNome.value.split(" ");
+        console.log(verificarNomeComposto);
+        if (verificarNomeComposto.length > 1) {
+            const formData = new FormData(this);
+
+            // No teste não foi apresentadpo nenhuma URL pro fetch
+            fetch('', {
+                method: 'post',
+                body: formData
+            }).then(function (response) {
+    
+                return response.text();
+    
+            }).then(function (text) {
+    
+                console.log(text)
+            }).catch(function (error) {
+                console.error(error);
+            })
+            alert('Enviado com sucesso!');
+            window.location.reload()
+        } else {
+               alert('Insira um nome e um sobrenome')
+        }
+    });
+    
+}
+
+envioDoFormulario();
